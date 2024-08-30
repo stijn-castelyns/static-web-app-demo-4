6d22000a511250872de406cdb950f4d1540e1462
stijn-castelyns <stijn@u2u.be>
2024-08-30 15:24:31 +0200

    New backend

---
+++ api/PirateBackend/PirateBackend1/PirateBackend1/Function1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace PirateBackend1
+{
+    public static class Function1
+    {
+        [FunctionName("pirate")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+           
+            return new OkObjectResult("Hello from azure function");
+        }
+    }
+}
